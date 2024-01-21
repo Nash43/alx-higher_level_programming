5a45e14ce0a3006f2ffcb3a647c6524a9bb2e828
Nash43 <nyashjovo@gmail.com>
2024-01-21 04:58:59 -0800

    print a function that determines a return value weather it is positive, negative, or equal to zero

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10, 10)
 
-number =float(input("number"))
+number = float(input("number"))
 if number < 0:
     print("is negative")
 elif number == 0:
     print("is zero")
 elif number > 0:
     print("is positive")
-
